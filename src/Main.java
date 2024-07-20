22f17fc8ff9fe47ee0501aa5a4f00a885ac70cc0
JUAN L P SEOANE <juan.seoane.solo@gmail.com>
2024-07-20 16:26:48 +0200

    Actualizacion desde Linux, para que funcione correctamente

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main {
   
     public static void main(String[] args) throws IOException {
         
-        try{
+/*        try{
             GraphiteLookAndFeel graphite = new GraphiteLookAndFeel();
             UIManager.setLookAndFeel(graphite);
         }catch(Exception e){
@@ -28,7 +28,7 @@ public class Main {
 
         window.run();
         window.setAlwaysOnTop(true);
-        window.setVisible(false);
+        window.setVisible(false); */
     // FIXME - 24-07-17 : Cuando cierras el VisorFCT y lo vuelves a abrir, habiendo seleccionado otra factura, no actualiza la Factura en el visor...¿?
     // FIXME - 24-07-01 : Falta en el VisorFCT : informar la 'Categoría' de la Factura y reemplazar Labels por TextFields
     // TODO - 24-07-01 : Falta en el VisorFCT : moverse a travéws de la tablaFCT con las flechas
